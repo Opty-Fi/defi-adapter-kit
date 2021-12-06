9f3e11ad59bec79026771f175acd1c8a3ad3ddfb
dhruvinparikh <imsilent93@gmail.com>
2021-12-06 16:58:37 -0500

    refactor(cleanup): use optyfi-defi legos

--- contracts/mock/Imports.sol
+++ contracts/mock/Imports.sol
@@ -4,3 +4,4 @@ pragma solidity ^0.6.12;
 pragma experimental ABIEncoderV2;
 
 import { IUniswapV2Pair } from "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+import { IHarvestController } from "@optyfi/defi-legos/ethereum/harvest.finance/contracts/IHarvestController.sol";
